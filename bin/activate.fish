46e7ea6df8d8b9a6080772a564709a5ec538e81a
voteme4 <zachberg2004@gmail.com>
2025-01-01 01:01:52 -0500

    Basic functionality implemented

--- bin/activate.fish
+++ bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/zach/Desktop/SpeedRead/SpeedRead"
+set -gx VIRTUAL_ENV "/home/zach/Desktop/PythonProjects/SpeedRead"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
