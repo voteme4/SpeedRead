30d2677c32464bb9faca138a3d80b9887406e285
voteme4 <zachberg2004@gmail.com>
2025-03-31 19:40:11 -0400

    Changed env to venv to not cause issues with .env stuff, reinstalled flask because it wasn't working right and now it is, made .flaskenv file because it's cool

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+#client = MongoClient('localhost', 27017)
 
 @app.route("/")
 
 def index():
-    return render_template("base_page.html")
+    return render_template("index.html")
 
-app.run(host="0.0.0.0", port = 5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 5000)
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Install
 python3 -m venv .
 source ./bin/activate
+
+# Run
+python3 app.py
